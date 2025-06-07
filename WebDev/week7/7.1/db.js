da78369bb3ab560cb4f58c790efc13ccd158c966
Anirudh Raturi <annigeek@gmail.com>
2025-06-07 15:33:03 +0530

    Added connnection with database

--- WebDev/week7/7.1/db.js
+++ WebDev/week7/7.1/db.js
@@ -1,27 +1,27 @@
 // to structure our code better we are going to put all our database logic here
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const ObjectId = mongoose.ObjectId
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
 
 //Schema
 const User = new Schema({
-    email : String,
-    password : String,
-    name : String
-})
+  email: { type: String, unique: true },
+  password: String,
+  name: String,
+});
 
 const Todo = new Schema({
-    title : String,
-    done : Boolean,
-    userId : ObjectId
-})
+  title: String,
+  done: Boolean,
+  userId: ObjectId,
+});
 
 //now we want something to perform operations when reqest will come => so we use model
-const UserModel = mongoose.model('users',User)
-const TodoModel = mongoose.model('todos',Todo)
+const UserModel = mongoose.model("users", User);
+const TodoModel = mongoose.model("todos", Todo);
 
 //now we are exporting a object which has key as UserModel and TodoModel so that we can import in index.js
-module.exports({
-    UserModel : UserModel,
-    TodoModel : TodoModel
-})
+module.exports = {
+  UserModel: UserModel,
+  TodoModel: TodoModel,
+};
--- WebDev/week7/7.1/index.js
+++ WebDev/week7/7.1/index.js
@@ -1,29 +1,99 @@
-const express = require('express')
-const {} = require('./db')
-const app = express()
+const express = require("express");
+const { UserModel, TodoModel } = require("./db");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "dfasdfasdf34356";
+const app = express();
 
-const PORT = 3000
+const PORT = 3000;
 
-app.post('signup',(req,res)=>{
+mongoose.connect(
+  "mongodb+srv://annigeek:re0f806Mm3iz8q0b@cluster0.8cyirhw.mongodb.net/todo-app-database1"
+);
 
-})
+app.use(express.json());
 
-app.post('/signin',(req,res)=>{
+//auth middleware
+function auth(req,res,next){
+  const token = req.headers.token
+  const decodedToken = jwt.verify(token,JWT_SECRET)
+  console.log(decodedToken.id)
+  if(decodedToken){
+    req.userId = decodedToken.id
+    next()
+  }else{
+    res.status(403).json({
+      msg : "Invalid Token"
+    })
+  }
+}
 
-})
+app.post("/signup", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
 
-app.post('/login',(req,res)=>{
+  await UserModel.create({
+    email: email,
+    password: password,
+    name: name,
+  });
 
-})
+  res.status(200).json({
+    msg: "You are logged in",
+  });
+});
 
-app.post('/todo',(req,res)=>{
+app.post("/signin", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-})
+  const user = await UserModel.findOne({
+    email: email,
+    password: password,
+  });
 
-app.get('/todos',(req,res)=>{
+  console.log(user);
 
-})
+  if (user) {
+    const token = jwt.sign(
+      {
+        id: user._id.toString(),
+      },
+      JWT_SECRET
+    );
 
-app.listen(3000,()=>{
-    console.log(`Server started at ${PORT}`)
-})
+    res.json({
+      token: token,
+    });
+    
+  } else {
+    res.status(403).json({
+      msg: "Invalid credentials",
+    });
+  }
+});
+
+app.post("/login", (req, res) => {});
+
+app.post("/todo",auth,async (req, res) => {
+  const userId = req.userId
+  const title = req.body.title
+
+  await TodoModel.create({
+    userId :userId,
+    title : title
+  })
+
+  res.json({
+    userId : userId,
+    title : title
+  })
+
+});
+
+app.get("/todos", (req, res) => {});
+
+app.listen(3000, () => {
+  console.log(`Server started at ${PORT}`);
+});
