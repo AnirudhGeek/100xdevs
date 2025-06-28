55754cfe082ae7b2d332bd9b3d62152ec1a431c0
Anirudh Raturi <annigeek@gmail.com>
2025-06-28 23:10:18 +0530

    i am drained today

---
+++ WebDev/week10/10.2/5iWasDrained/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <img style={{marginLeft:"65%"}} src="https://pbs.twimg.com/media/GRDF1TdWsAAYtu5.jpg" alt="" />
+    </div>
+  )
+}
+
+export default App
