784973a091584f94da5c3db96931e7cd74a4f506
Anirudh Raturi <annigeek@gmail.com>
2025-09-04 23:26:07 +0530

    setup

---
+++ WebDev/week14/More more Ts/1.setup/1app.js
@@ -0,0 +1,6 @@
+var userName = "Anirudh";
+var age = 30;
+var college = "COER";
+console.log(userName);
+console.log(age);
+console.log(college);
---
+++ WebDev/week14/More more Ts/1.setup/1app.ts
@@ -0,0 +1,6 @@
+var userName:string = "Anirudh"
+var age : number = 30
+var college: string = "COER"
+console.log(userName)
+console.log(age)
+console.log(college)
