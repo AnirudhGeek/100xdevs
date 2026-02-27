ac3aa72be07cae06c5ee8e084a759803e879d208
Anirudh Raturi <annigeek@gmail.com>
2026-02-27 09:52:23 +0530

    ping-pong WebSockets

---
+++ WebDev/week16/1Websockets/ws-frontend/src/App.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useRef, useState, type RefObject } from "react"
+
+
+const App = () => {
+
+  const [socket,setSocket] = useState()
+  const inputRef = useRef()
+
+
+  function sendMessage(){
+    if(!socket){
+      return
+    }
+    const message = inputRef.current.value
+    socket.send(message)
+  }
+
+  useEffect(()=>{
+    const ws = new WebSocket("ws://localhost:8080")
+    setSocket(ws)
+
+    ws.onmessage = (ev)=>{
+      alert(ev.data)
+    }
+
+  },[])
+  return (
+    <div>
+      <input ref={inputRef} type="text" placeholder="Message..." />
+      <button onClick={sendMessage}>Send</button>      
+    </div>
+  )
+}
+
+export default App
