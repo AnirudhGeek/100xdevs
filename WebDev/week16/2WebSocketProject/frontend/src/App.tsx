42a158f4048ee9c5259437a56b8faa8d7033f035
Anirudh Raturi <annigeek@gmail.com>
2026-03-02 23:11:45 +0530

    frontend connection with WebSockets

--- WebDev/week16/2WebSocketProject/frontend/src/App.tsx
+++ WebDev/week16/2WebSocketProject/frontend/src/App.tsx
@@ -1,19 +1,65 @@
+  import { useEffect, useRef, useState } from "react";
 
 
-const App = () => {
-  return (
-    <div className=" w-screen flex flex-col justify-between">
-      <div className="h-[80vh]   bg-gray-400">asdfas</div>
-      <div className="flex items-center gap-5 p-2">
-        <input
-          type="text"
-          className="bg-white text-lg px-1 py-2 text-black w-full border"
-          placeholder="Enter your message..."
-        />
-        <button className="bg-blue-400 text-2xl p-2 text-white rounded">Send</button>
+  const App = () => {
+
+    const [messages , setMessages] = useState(["hey there"])
+    // const [inputValue , setInputValue]
+    const inputRef = useRef<HTMLInputElement>(null)
+    const wsRef = useRef<WebSocket>(null)
+
+    useEffect(()=>{
+      const ws = new WebSocket("ws://localhost:8080")
+
+      ws.onmessage = (ev)=>{
+        setMessages(m=>[...m,ev.data])
+      }
+
+      wsRef.current = ws
+
+      ws.onopen = ()=>{
+        ws.send(JSON.stringify({
+          "type" : "join",
+          "payload" : {
+            "roomId" : "red"
+          }
+        }))
+      }
+
+
+      return()=>{
+        ws.close()
+      }
+    },[])
+
+    return (
+      <div className=" w-screen flex flex-col justify-between bg-gray-900">
+        <div className="h-[90vh]">
+          {
+            messages.map(message=>(
+              <div className="bg-white px-2 py-4 w-fit rounded m-4">{message}</div>
+            ))
+          }
+        </div>
+        <div className="flex items-center gap-5 p-2">
+          <input
+            type="text"
+            className="bg-white text-lg px-1 py-2 text-black w-full border"
+            placeholder="Enter your message..."
+            ref={inputRef}
+          />
+          <button onClick={()=>{
+            wsRef.current?.send(JSON.stringify({
+              "type" : "chat",
+              "payload" : {
+                "message" : inputRef.current?.value
+              }
+            }))
+            inputRef.current ? inputRef.current.value = "" : null
+          }}  className="bg-blue-400 text-2xl p-2 text-white rounded">Send</button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default App;
+  export default App;
