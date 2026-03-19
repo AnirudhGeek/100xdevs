ebcd569a50114f98676e39cee6f86c955f7a7d19
Anirudh Raturi <annigeek@gmail.com>
2026-03-19 23:54:36 +0530

    integrated prisma in NextJs Dashboard

--- WebDev/week19.1/dashboard/app/api/signup/route.ts
+++ WebDev/week19.1/dashboard/app/api/signup/route.ts
@@ -1,10 +1,33 @@
+import { prisma } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function POST(req : NextRequest){
-    const {username , password} = await req.json()
+export async function POST(req: NextRequest) {
+  const { username, password } = await req.json();
+
+
+  try {
+      const user = await prisma.user.create({
+    data: {
+      username,
+      password,
+    },
+  });
+
+  if (user) {
     return NextResponse.json({
-        username,
-        msg : "user signed-up successfull!"
+      username,
+      msg: "User signed-up successfull!",
+    });
+  }
+
+
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({
+        msg : "User already exist!"
     })
+  }
+
+
 }
